b318aa22c19f139d16bac51a68bf5f76a4b0d2f0
Mitchell shelton <leafytwig@hotmail.com>
2018-03-30 20:12:43 -0600

    Add background music/script

---
+++ Assets/Scripts/BGmusic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGmusic : MonoBehaviour {
+	static BGmusic instance = null;
+
+	void Awake(){
+		if (instance != null) {
+			Destroy (gameObject);
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad (gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
